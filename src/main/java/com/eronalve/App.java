ccab4b63b0aece1a5d978edec9ad15613cf1cf2c
EronAlves1996 <eron.alves@rocketmail.com>
2024-02-05 22:07:25 -0300

    feat: create impl

--- src/main/java/com/eronalve/App.java
+++ src/main/java/com/eronalve/App.java
@@ -1,6 +1,11 @@
 package com.eronalve;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -17,6 +22,19 @@ public class App {
   private static class Playlist {
     String name;
     List<Song> songs;
+
+    List<Song> filterForGenre(String genre) {
+      Function<List<Song>, Stream<Song>> toStream = List::stream;
+
+      return Optional.ofNullable(songs)
+          .map(toStream
+              .andThen(s -> s
+                  .filter(song -> Optional.ofNullable(song.genre)
+                      .filter(g -> g.equals(genre))
+                      .isPresent())
+                  .collect(Collectors.toList())))
+          .orElse(Collections.emptyList());
+    }
   }
 
   public static void main(String[] args) {
