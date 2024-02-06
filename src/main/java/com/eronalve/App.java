c35a9de94fe286a92f8400a7274cb59cc29ee633
EronAlves1996 <eron.alves@rocketmail.com>
2024-02-05 21:58:44 -0300

    feat: setup basic infrastructure

--- src/main/java/com/eronalve/App.java
+++ src/main/java/com/eronalve/App.java
@@ -1,10 +1,24 @@
 package com.eronalve;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App {
+
+  private static class Song {
+    String title;
+    String artist;
+    String genre;
+  }
+
+  private static class Playlist {
+    String name;
+    List<Song> songs;
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
